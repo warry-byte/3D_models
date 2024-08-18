d9880ff95a512fccf86fd0e74d502f08074749ac
warry-byte <antoun.aj@gmail.com>
2024-08-18 23:30:07 +0200

    ENH: Testing adding quadrinities everywhere.

--- models/quadrinity/quadrinity_asm.py
+++ models/quadrinity/quadrinity_asm.py
@@ -9,5 +9,6 @@ set_global_fn(10)
 combined_model = quadrinity()
 
 for i in range(10):
-    combined_model += quadrinity().translateZ((3*np.sqrt(6)/3) * i)
-    combined_model.save_as_scad()
+    combined_model += quadrinity().translateX((3) * i)
+    combined_model += quadrinity().translateY((-3*np.sqrt(6)/3)).translateX(3/2+3*i)
+combined_model.save_as_scad()
