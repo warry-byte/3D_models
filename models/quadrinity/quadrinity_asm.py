273d589f867862ff6d4bcf632d90b5017e47ad15
warry-byte <antoun.aj@gmail.com>
2024-08-18 23:36:33 +0200

    ENH: New arrangement with second row entangled in a crystal structure...

--- models/quadrinity/quadrinity_asm.py
+++ models/quadrinity/quadrinity_asm.py
@@ -10,5 +10,5 @@ combined_model = quadrinity()
 
 for i in range(10):
     combined_model += quadrinity().translateX((3) * i)
-    combined_model += quadrinity().translateY((-3*np.sqrt(6)/3)).translateX(3/2+3*i)
+    combined_model += quadrinity().rotateZ(180).translateY(-3 * np.sqrt(3) / 3 + 3 * np.sqrt(3) / 6).translateX(3/2+3*i)
 combined_model.save_as_scad()
