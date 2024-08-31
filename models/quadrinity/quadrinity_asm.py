8d7e737be137a2735ad6647528c7d64c6821adf0
warry-byte <antoun.aj@gmail.com>
2024-09-01 01:04:02 +0200

    ENH: Added quadrinity that is rotated around an edge (corner that is in X < 0 and y < 0 and that which is in X = 0, Z = 0)

--- models/quadrinity/quadrinity_asm.py
+++ models/quadrinity/quadrinity_asm.py
@@ -3,12 +3,36 @@ import numpy as np
 from quadrinity import quadrinity
 from solid2 import *
 
+from utils.utils import get_object_rotated_around_edge
+
 quad_list = []
 set_global_fn(10)
 
 combined_model = quadrinity()
 
-for i in range(10):
-    combined_model += quadrinity().translateX((3) * i)
-    combined_model += quadrinity().rotateZ(180).translateY(-3 * np.sqrt(3) / 3 + 3 * np.sqrt(3) / 6).translateX(3/2+3*i)
+for x in range(2):
+    combined_model += quadrinity().translateX(3 * x)
+
+
+# Adding top of the y triangle
+combined_model += quadrinity().translateX(3/2).translateY(-3 * np.sqrt(3) / 2)  # Translate Y by the height of a face
+
+# Adding inner quadrinity, inside the formed quadrinity trinity
+# axis = [3 * np.sqrt(3) / 2, 3/2, 0]
+# axis = np.asarray([0, -3 * np.sqrt(3) / 3, 0]) - np.asarray([-3 / 2, 3 * np.sqrt(3) / 6, 0])
+vertex2 = np.asarray([0, -3 * np.sqrt(3) / 3, 0])
+vertex1 = np.asarray([-3 / 2, 3 * np.sqrt(3) / 6, 0])
+axis = vertex2 - vertex1
+mid_point = (vertex1 - vertex2) / 2
+# check where is the middle point (debug)
+# combined_model += sphere(0.1).translate(mid_point + vertex2)
+# center_coord = np.asarray([0, 0, 3 * np.sqrt(6) / 12])
+interior_quad = quadrinity().translate(-mid_point-vertex2).rotate(v=axis, a=70.53).translate(mid_point+vertex2)
+# interior_quad = (quadrinity().rotate(a=45, v=axis))
+                 #.rotateY(180).translateY(-3 * np.sqrt(3) / 3 + 3 * np.sqrt(3) / 6).translateX(3/2).translateZ(3 * np.sqrt(6) / 3))
+combined_model += interior_quad
+
+# Adding top quadrinity in Z
+# combined_model += quadrinity().translateY(-3 * np.sqrt(3) / 3 + 3 * np.sqrt(3) / 6).translateX(3/2).translateZ(3 * np.sqrt(6) / 3)
+
 combined_model.save_as_scad()
